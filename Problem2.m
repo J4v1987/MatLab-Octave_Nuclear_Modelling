d9a1d43ee72b3039cbb7d9b351615cb4b3488eba
Javier987 <96040240+J4v1987@users.noreply.github.com>
2026-02-03 19:12:38 +0100

    Fix unit display in detection yield output

--- Problem2.m
+++ Problem2.m
@@ -295,7 +295,7 @@ disp(" ");
 disp("∵ ""Detection yield"", Y, is understood as the arithmetic product of detection efficiency, ηₑ, and analytical cylinder volume, Vₐ. ");
 disp("→ Y = ηₑ⋅Vₐ");
 detectionYield=detectionEfficiency*analyticalVolume;
-disp(sprintf("→ Y = (%.5f[e⁻])⋅(%.5f[e⁻]) [mm³]", detectionEfficiency, analyticalVolume));
+disp(sprintf("→ Y = (%.5f[e⁻])⋅(%.5f[mm³])", detectionEfficiency, analyticalVolume));
 disp(sprintf("⟹ Y = %.5f [mm³]", detectionYield));
 
 %[EXTERNAL RAW DATA]
